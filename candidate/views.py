15c3ad86b99e195606127679a3e09a5c0cdf7e76
Manish Dheke <143415109+Manish-loop@users.noreply.github.com>
2024-11-26 20:38:40 +0545

    Commits

--- candidate/views.py
+++ candidate/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from hr.models import JobPost, CandidateApplication
 # Create your views here.
+
+@login_required
+def candidate_dashboard(request):
+    jobs = JobPost.objects.all()
+    print(jobs)
+    return render(request,'candidate/dashboard.html',{'jobs':jobs})
+
+@login_required
+def myJobListviews(request):
+    return render(request,'candidate/myjoblist.html')
+
+@login_required
+def applyforjob(request,pk):
+    print(pk)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('name')
+        college = request.POST.get('college')
+        passing_year = request.POST.get('passing_year')
+        yearOfExperience = request.POST.get('yearOfExperience')
+        resume = request.FILES.get('resume')
+        job = JobPost.objects.get(id=pk)
+        CandidateApplication(user=request.user, job=job,passingYear=passing_year,yearOfExperience=yearOfExperience,resume=resume).save()
+        return redirect('candidate_dashboard')
+    return render(request,'candidate/apply.html')
--- hr/views.py
+++ hr/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-
-from hr.models import JobPost
+from hr.models import JobPost, CandidateApplication, SelectCandidateJob
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def hrHome_views(request):
-    return render(request, 'hr/hrdashboard.html')
+    
+    return render(request, 'hr/hrdashboard.html',{'jobs':jobs})
 
 # def hrHome(request):
 #     # Fetch job posts to display on the dashboard
@@ -15,9 +17,23 @@ def hrHome_views(request):
 #     }
 #     return render(request, 'hr/hrdashboard.html', context)
 
+@login_required
 def post_job_views(request):
-    return render(request, 'hr/postjob.html')
+    msg = None
+    if request.method == 'POST':
+        job_title = request.POst.get('job-title')
+        address = request.POST.get('address')
+        company_name = request.POST.get('company-name')
+        salary_low = request.POST.get('salary-low')
+        salary_high = request.POST.get('salary-high')
+        last_date = request.POST.get('last-date')
+        print(job_title+" "+address+" "+company_name)
+        msg = "Job added successfully"
+        job_post = JobPost(user=request.user,title=job_title,address=address,companyName=company_name,salaryHigh=salary_high,salaryLow=salary_low,lastDateToApply=last_date)
+        job_post.save()
+    return render(request, 'hr/postjob.html',{'msg':msg})
 
+@login_required
 def candidate_view(request,pk):
     print(pk)
     return render(request, 'hr/candidate.html')
